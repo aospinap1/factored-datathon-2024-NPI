24bf03284f447ba9ef011d5046bfc14d1889bfda
aospinap1 <aospinap1@eafit.eud.co>
2024-08-20 20:19:25 -0500

    Store files in S3

--- src/constants.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-main_dir = os.path.dirname(sys.path[0])
-paths = {
-    "src": os.path.join(main_dir, "src"),
-    "data": os.path.join(main_dir, "data"),
-    "config": os.path.join(main_dir, "config")
-}
